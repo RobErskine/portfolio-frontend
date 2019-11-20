4bb849073ed91d6f5d866397f4e2b8f8010a6802
Rob Erskine <Rob@RobErskine.com>
2019-11-19 20:41:27 -0500

    adding in links to the list item

--- components/blog/listItem.vue
+++ components/blog/listItem.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="m_blog-list-item" v-bind:style="{ backgroundColor: post.backgroundColor }">
-        <h4 v-bind:style="{ color: post.fontColor }">{{post.title}}</h4>
+        <nuxt-link :to="post.uri">
+            <h4 v-bind:style="{ color: post.fontColor }">{{post.title}}</h4>
+        </nuxt-link>
         <time v-bind:style="{ color: post.fontColor, borderTopColor: post.fontColor }">{{post.postDate}}</time>
     </div>
 </template>
@@ -10,6 +12,14 @@
         padding:1.5em 2em;
         border-radius:1em 2em;
         overflow:hidden;
+        a{
+            text-decoration: none;
+            opacity:1;
+            &:hover{
+                text-decoration: none;
+                opacity:0.8;
+            }
+        }
         h4{
             font-size:2.5vw;
             line-height:1.1;
