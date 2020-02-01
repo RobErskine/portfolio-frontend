7530fd70a8c42ea4262e53a1d174ee9733b7337b
Rob Erskine <Rob@RobErskine.com>
2020-02-01 13:54:38 -0500

    adding in resume canvas

--- components/animations/resumecanvas.vue
+++ components/animations/resumecanvas.vue
@@ -18,9 +18,55 @@ section.resume.intro{
             let resumeIllo = new Zdog.Illustration({
                 element: '.resume-canvas',
                 dragRotate: true,
-                rotate: {y: -TAU/14, x: -TAU/30},
-                scale: 0.75
             });
+
+            let dome = new Zdog.Hemisphere({
+            addTo: resumeIllo,
+            diameter: 400,
+            // fill enabled by default
+            // disable stroke for crisp edge
+            stroke: false,
+            color: '#ffbc53',
+            backface: '#f89943',
+            rotate: { x: -TAU / 4},
+            translate: {y: -300}
+            });
+
+            let stalk = new Zdog.Cylinder({
+            addTo: resumeIllo,
+            diameter: 120,
+            length: 150,
+            stroke: false,
+            color: '#ffbc53',
+            rotate: {x: -TAU / 4},
+            translate: {y:-50}
+            });
+
+            let box = new Zdog.Box({
+            addTo: resumeIllo,
+            width: 250,
+            height: 150,
+            depth: 250,
+            stroke: 20,
+            color: '#cd4527',
+            translate: {y: 100}
+            });
+
+            var ring = new Zdog.Ellipse({
+            addTo: resumeIllo,
+            diameter: 120,
+            quarters: 2,
+            translate: { x: 160, y: -200 },
+            rotate: {z: TAU/12},
+            color: '#ffbc53',
+            stroke: 30,
+            backface: false,
+            });
+
+            ring.copy({
+            translate: { x: -160, y: -200 },
+            rotate: {z: 0, z: TAU / 2.5},
+            })
 
             function animate() {
                 resumeIllo.updateRenderGraph();
