e858798628ea3a4a389c14ebe4f889f87a0dd9f2
Rob Erskine <Rob@RobErskine.com>
2020-02-05 21:25:49 -0500

    resume content

--- pages/resume/index.vue
+++ pages/resume/index.vue
@@ -3,7 +3,38 @@
         <Logo size="size-small" position="position-left"/>
         <sectionHeader post-title="Résumé" class-name="resume" canvas="./animations/resumecanvas"></sectionHeader>
         <section class="block copy">
-            <h4>here is content</h4>
+            <aside>
+                <div v-for="skill in entries[0].skills">
+                    <h3>{{skill.skillTitle}}</h3>
+                    <div v-html="skill.skillsList"></div>
+                </div>
+            </aside>
+            <div>
+                <div>
+                    <h2>Experiences</h2>
+                    <div v-for="experience in entries[0].experiences">
+                        <h3>{{experience.resumeTitle}}</h3>
+                        <time>{{experience.dateStart}} - {{experience.dateEnd}}</time>
+                        <div v-html="experience.description"></div>
+                    </div>
+                </div>
+                <div>
+                    <h2>Awards</h2>
+                    <div v-for="award in entries[0].awards">
+                        <h3>{{award.awardTitle}}</h3>
+                        <time>{{award.dateStart}}</time>
+                        <div v-html="award.description"></div>
+                    </div>
+                </div>
+                <div>
+                    <h2>Passion Projects</h2>
+                    <div v-for="project in entries[0].passionProjects">
+                        <h3>{{project.projectTitle}}</h3>
+                        <time>{{project.dateStart}}</time>
+                        <div v-html="project.description"></div>
+                    </div>
+                </div>
+            </div>
         </section>
     </div>
 </template>
@@ -28,7 +59,11 @@ export default {
         return {
             title: 'Resume | Rob Erskine - Creative Developer',
             description: 'Resume for Rob Erskine, including advertising, marketing, user experience, design, and development work.',
-            image: 'https://placehold.it/1200x630?text=TODO'
+            image: 'https://placehold.it/1200x630?text=TODO',
+            // awards: entries[0].awards,
+            // experiences: entries[0].experiences,
+            // passionProjects: entries[0].passionProjects,
+            // skills: entries[0].skills
         }
     },
     head (){
