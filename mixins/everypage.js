caa539eb254141f3b20834de753f99a06f0bebc2
Rob Erskine <Rob@RobErskine.com>
2020-06-28 20:25:30 -0400

    fixing syntax error in everypage

--- mixins/everypage.js
+++ mixins/everypage.js
@@ -1,7 +1,7 @@
 export default {
     externalLinks: function () {
         setTimeout(function(){
-            $('body').find("p a[href^='http'],li a[href^='http'], blockquote a[href^='http'], , figcaption a[href^='http']").each(function() {
+            $('body').find("p a[href^='http'],li a[href^='http'], blockquote a[href^='http'], figcaption a[href^='http']").each(function() {
                 $(this).prepend('<span class="favicon" aria-hidden="true" style="background-image:url(https://www.google.com/s2/favicons?domain=' + this.hostname + ')" ></span>');
                 $(this).attr('target','_blank');
             });
