3da944c79d12d1805e8b10d3c5d2ad901a2bbd13
Rob Erskine <Rob@RobErskine.com>
2020-08-07 16:16:18 -0400

    twitter bust cache

--- pages/index.vue
+++ pages/index.vue
@@ -19,9 +19,9 @@ export default {
                 { hid: 'twitter:title', name: 'twitter:title', content: 'Rob Erskine - Creative Developer' },
                 { hid: 'description', name: 'description', content: 'Creative designer and developer obsessed with solving complex digital problems.' },
                 { hid: 'og:description', name: 'og:description', content: 'Hey there 👋 I’m a creative designer, developer, and leader obsessed with solving complex problems' },
-                { hid: 'og:image', name: 'og:image', content: 'https://roberskine.com/images/homepage-share.png' },
+                { hid: 'og:image', name: 'og:image', content: 'https://roberskine.com/images/homepage-share.png?v2' },
                 { hid: 'twitter:card', name:"twitter:card", content:"summary_large_image"},
-                { hid: 'twitter:image', name: 'twitter:image', content: 'https://roberskine.com/images/homepage-twitter-share.png' }
+                { hid: 'twitter:image', name: 'twitter:image', content: 'https://roberskine.com/images/homepage-twitter-share.png?v2' }
             ]
         }
     },
