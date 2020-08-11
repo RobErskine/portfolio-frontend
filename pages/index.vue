fd84d2962795fa576ed190378f54d802d0e2bae0
Rob Erskine <Rob@RobErskine.com>
2020-08-11 10:46:39 -0400

    adding in homepage linkedin sharing

--- pages/index.vue
+++ pages/index.vue
@@ -17,9 +17,12 @@ export default {
             meta: [
                 { hid: 'og:title', name: 'og:title', content: 'Rob Erskine - Creative Developer' },
                 { hid: 'twitter:title', name: 'twitter:title', content: 'Rob Erskine - Creative Developer' },
-                { hid: 'description', name: 'description', content: 'Creative designer and developer obsessed with solving complex digital problems.' },
+                { hid: 'description', name: 'description', content: 'Creative designer, developer, and leader obsessed with solving complex digital problems.' },
                 { hid: 'og:description', name: 'og:description', content: 'Hey there 👋 I’m a creative designer, developer, and leader obsessed with solving complex problems' },
                 { hid: 'og:image', name: 'og:image', content: 'https://roberskine.com/images/homepage-share.png?v2' },
+                { hid: 'og:type', name: 'og:type', content: 'website'},
+                { hid: 'og:title', name: 'og:title', content: 'Rob Erskine - Creative Development'},
+                { hid: 'og:url', 'name': 'og:url', content: 'https://roberskine.com'},
                 { hid: 'twitter:card', name:"twitter:card", content:"summary_large_image"},
                 { hid: 'twitter:image', name: 'twitter:image', content: 'https://roberskine.com/images/homepage-twitter-share.png?v2' }
             ]
